524db477377846f28d8fd9affa66a911cb5d4405
Carlos Sanchez <caaugustoss@gmail.com>
2020-03-26 12:44:09 +0100

    Transforming binary nodes

--- src/main/scala/com/ubirch/services/pm/ProtocolMessageService.scala
+++ src/main/scala/com/ubirch/services/pm/ProtocolMessageService.scala
@@ -1,19 +1,22 @@
 package com.ubirch.services.pm
 
-import java.util.{ Base64, UUID }
+import java.util.{Base64, UUID}
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.node.{ArrayNode, BinaryNode, ContainerNode, ObjectNode, TextNode}
 import com.typesafe.scalalogging.LazyLogging
 import com.ubirch.client.protocol.DefaultProtocolVerifier
 import com.ubirch.crypto.GeneratorKeyFactory
 import com.ubirch.crypto.utils.Curve
 import com.ubirch.protocol.codec.MsgPackProtocolDecoder
-import com.ubirch.protocol.{ ProtocolException, ProtocolMessage }
+import com.ubirch.protocol.{ProtocolException, ProtocolMessage}
 import javax.inject._
 import org.apache.commons.codec.binary.Hex
 import org.json4s.Formats
 import org.json4s.jackson.JsonMethods.fromJsonNode
+import scala.collection.JavaConverters._
 
-import scala.util.{ Failure, Success, Try }
+import scala.util.{Failure, Success, Try}
 
 @Singleton
 class ProtocolMessageService @Inject() (implicit formats: Formats) extends LazyLogging {
@@ -48,6 +51,25 @@ class ProtocolMessageService @Inject() (implicit formats: Formats) extends LazyL
     }
   }
 
+  def replaceBinaryNodesWithTextNodes(tree: ContainerNode[_]): Unit = {
+    def inner[K](withKeys: (K => Unit) => Unit, get: K => JsonNode, set: (K, JsonNode) => Unit): Unit = {
+      withKeys { key =>
+        get(key) match {
+          case binary: BinaryNode => set(key, new TextNode(binary.asText()))
+          case containerNode: ContainerNode[_] => replaceBinaryNodesWithTextNodes(containerNode)
+          case _ => set(key, new TextNode("Holla"))
+        }
+      }
+    }
+
+    tree match {
+      case o: ObjectNode => inner[String](o.fieldNames().asScala.foreach, o.get, o.set)
+      case a: ArrayNode => inner[Int]((0 until a.size()).foreach, a.get, a.set)
+    }
+
+    tree
+  }
+
   def unpackFromBytes[T: Manifest](bytes: Array[Byte]): Try[UnPacked[T]] = {
     (for {
 
@@ -57,7 +79,9 @@ class ProtocolMessageService @Inject() (implicit formats: Formats) extends LazyL
       decoder = MsgPackProtocolDecoder.getDecoder
       pm <- Try(decoder.decode(bytes))
 
-      payloadJValue <- Try(fromJsonNode(pm.getPayload))
+      payload <- Try(pm.getPayload)
+      _ <- Try(replaceBinaryNodesWithTextNodes(payload.asInstanceOf[ContainerNode[_]]))
+      payloadJValue <- Try(fromJsonNode(payload))
       _ = logger.info("protocol_message_payload_json_node {}", pm.getPayload.toString)
       _ = logger.info("protocol_message_payload {}", payloadJValue.toString)
       pt <- Try(payloadJValue.extract[T])
