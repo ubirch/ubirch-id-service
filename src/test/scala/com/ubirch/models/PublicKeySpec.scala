2304bd3040ab5a68b1f84c3038caaeae331f6949
Carlos Sanchez <caaugustoss@gmail.com>
2020-03-26 12:08:21 +0100

    Revert "Adding log for json node"
    This reverts commit fb3c5fde

--- src/test/scala/com/ubirch/models/PublicKeySpec.scala
+++ src/test/scala/com/ubirch/models/PublicKeySpec.scala
@@ -1,17 +1,14 @@
 package com.ubirch.models
 
-import java.util.{Base64, UUID}
+import java.util.{ Base64, UUID }
 
 import com.ubirch.TestBase
 import com.ubirch.crypto.GeneratorKeyFactory
-import com.ubirch.services.formats.{JsonConverterService, JsonFormatsProvider}
+import com.ubirch.services.formats.{ JsonConverterService, JsonFormatsProvider }
 import com.ubirch.services.key.PubKeyVerificationService
 import com.ubirch.services.pm.ProtocolMessageService
-import com.ubirch.util.{DateUtil, PublicKeyUtil}
-import org.apache.commons.codec.binary.Hex
+import com.ubirch.util.{ DateUtil, PublicKeyUtil }
 import org.joda.time.format.ISODateTimeFormat
-import org.json4s.Extraction
-import org.json4s.JsonAST.{JInt, JString}
 
 import scala.util.Try
 
@@ -75,27 +72,6 @@ class PublicKeySpec extends TestBase {
       go(PublicKeyUtil.EDDSA)
 
       go(PublicKeyUtil.ECDSA)
-
-    }
-
-
-    "parse from device" in {
-      val input = """{"hwDeviceId": "UBIR<q\xbf\x88\x1f\xa4<q\xbf\x88\x1f\xa4", "pubKey": "\xe8\xb1X:yY\x11\xbaH!\xf6\xd1YW\xfb\xb2\xa8\xb6X\x9f\xac\x0e(\xf0F\x9f\x1d]FN\x179", "pubKeyId": "\xe8\xb1X:yY\x11\xbaH!\xf6\xd1YW\xfb\xb2\xa8\xb6X\x9f\xac\x0e(\xf0F\x9f\x1d]FN\x179", "algorithm": "ECC_ED25519", "created": 1560520400, "validNotAfter": 1591278800, "validNotBefore": 1560520400}"""
-      jsonConverter.toJValue(input) match {
-        case Right(value) =>
-          val newValue = value.mapField {
-            case ("created", JInt(num)) => ("created", JString(DateUtil.ISOFormatter.print(num.toLong * 1000)))
-            case ("validNotAfter", JInt(num)) => ("validNotAfter", JString(DateUtil.ISOFormatter.print(num.toLong * 1000)))
-            case ("validNotBefore", JInt(num)) => ("validNotBefore", JString(DateUtil.ISOFormatter.print(num.toLong * 1000)))
-            case ("pubKey", JString(key)) => ("pubKey", JString(Base64.getEncoder.encodeToString(key.getBytes())))
-            case x => x
-          }
-
-          println(newValue)
-
-          Extraction.extract[PublicKeyInfo](newValue)
-        case Left(value) => fail(value)
-      }
 
     }
 
