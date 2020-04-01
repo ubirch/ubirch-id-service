bfa81c2c664e5b2c67940d65e029b2031e4bef55
sharkgua <benoit.george@ubirch.com>
2020-04-01 10:03:18 +0200

    Updated swagger doc

--- src/main/scala/com/ubirch/controllers/SwaggerElements.scala
+++ src/main/scala/com/ubirch/controllers/SwaggerElements.scala
@@ -8,6 +8,8 @@ object SwaggerElements {
   val TAG_KEY_SERVICE = "key-service"
   val TAG_KEY_REGISTRY = "key registry"
   val TAG_MSG_PACK = "MessagePack"
+  val TAG_WELCOME = "Welcome"
+  val TAG_HEALTH = "Health"
 
   val ERROR_RESPONSE: String = "version: '1.0' status: NOK\n" + "errorType: FindTrustedError\n" + "message: failed to query trusted keys"
 }
