2116e6f70cab38a924ce0f1785eafb148e999ac2
lotta <lotta.rueger@ubirch.com>
2020-05-04 09:06:47 +0200

    adding a start test environment script and changing docker-compose a bit

---
+++ startTestEnv.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker stop $(docker ps -a -q)
+kill -9 $(lsof -t -i:9092)
+sleep 5s;
+docker-compose up
+
+function cleanup() {
+docker stop $(docker ps -a -q)
+kill -9 $(lsof -t -i:9092)
+}
+
+trap cleanup EXIT
