eff698a69c4fc492e456d419f539ac881b6f3edc
lotta <lotta.rueger@ubirch.com>
2020-05-04 09:07:59 +0200

    auto formatting shell script

--- startTestEnv.sh
+++ startTestEnv.sh
@@ -2,12 +2,12 @@
 
 docker stop $(docker ps -a -q)
 kill -9 $(lsof -t -i:9092)
-sleep 5s;
+sleep 5s
 docker-compose up
 
 function cleanup() {
-docker stop $(docker ps -a -q)
-kill -9 $(lsof -t -i:9092)
+  docker stop $(docker ps -a -q)
+  kill -9 $(lsof -t -i:9092)
 }
 
 trap cleanup EXIT
